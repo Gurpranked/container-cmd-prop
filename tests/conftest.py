86e31e8ab041a84e956ee360009fb7ea102aa585
Nicholas Hubbard <nicholashubbard@posteo.net>
2025-11-07 13:08:35 -0500

    switched test base image to alpine:latest because its faster

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,18 +4,18 @@
 import pytest
 
 @pytest.fixture
-def dockerfile_ubuntu_latest(tmp_path):
-    """Fixture that provides a hardcoded Dockerfile for Ubuntu latest.
+def dockerfile_alpine_latest(tmp_path):
+    """Fixture that provides a hardcoded Dockerfile for alpine:latest.
     The CMD used simply prints out the standard /etc/os-release file.
     """
     dockerfile = tmp_path / "Dockerfile"
-    dockerfile.write_text("FROM ubuntu:latest\nCMD [\"cat\",\"/etc/os-release\"]\n")
+    dockerfile.write_text("FROM alpine:latest\nCMD [\"cat\",\"/etc/os-release\"]\n")
     return dockerfile
 
 @pytest.fixture
-def containerfile_ubuntu_latest(dockerfile):
-    """Fixture that provides a hardcoded Containerfile for Ubuntu latest.
-    This fixture is equivalent to the `dockerfile_ubuntu_latest` fixture, except
+def containerfile_alpine_latest(dockerfile):
+    """Fixture that provides a hardcoded Containerfile for alpine:latest.
+    This fixture is equivalent to the `dockerfile_alpine_latest` fixture, except
     that the Path it gives back has basename Containerfile.
     """
     containerfile = dockerfile.replace(dockerfile.parent / "Containerfile")
